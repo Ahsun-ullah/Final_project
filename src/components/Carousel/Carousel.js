7d98dde9c206ce9d60ea1f0e2a2b96f87c9d2530
ahsun rahim <ahsunrahim@gmail.com>
2022-12-31 02:15:23 +0600

    implement carosel using swiperjs

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,84 +1,48 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Autoplay } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import loginImg from '../../Assets/loginImg.png'
+import registerImg from '../../Assets/registerImg.png'
+import git from '../../Assets/git.png'
 
 const Carousel = () => {
   return (
     <div>
       <div>
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide relative"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-          <div className="carousel-inner relative w-full overflow-hidden">
-            <div className="carousel-item active float-left w-full">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-                className="block w-full"
-                alt="Wild Landscape"
-              />
-            </div>
-            <div className="carousel-item float-left w-full">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-                className="block w-full"
-                alt="Camera"
-              />
-            </div>
-            <div className="carousel-item float-left w-full">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-                className="block w-full"
-                alt="Exotic Fruits"
-              />
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon inline-block bg-no-repeat"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon inline-block bg-no-repeat"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
+      <h2 className=' flex font-bold text-black justify-center text-4xl py-10'>
+        This is home
+      </h2>
+      </div>
+      <Swiper
+        className="mySwiper h-80 w-auto bg-gray-400"
+        spaceBetween={10}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+      >
+        <SwiperSlide>
+          <img src={loginImg} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={registerImg} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={git} alt="" />
+        </SwiperSlide>
+      </Swiper>
+      <div className='h-screen'>
+
       </div>
     </div>
   )
